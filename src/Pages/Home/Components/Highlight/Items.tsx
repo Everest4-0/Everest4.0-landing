59c7123ba8146ad2de03c37a087f9fe129690b3d
Sebastiao Lutonda <lutonda@gmail.com>
2024-02-03 19:28:00 +0100

    carousel and higlinht

---
+++ src/Pages/Home/Components/Highlight/Items.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import pro1 from '../../../../assets/images/highlight/pro1.jpeg'
+import pro2 from '../../../../assets/images/highlight/pro2.jpeg'
+import pro3 from '../../../../assets/images/highlight/pro3.jpeg'
+import pro4 from '../../../../assets/images/highlight/pro4.jpeg'
+import pro5 from '../../../../assets/images/highlight/pro5.jpeg'
+import pro6 from '../../../../assets/images/highlight/pro6.jpeg'
+import vro1 from '../../../../assets/images/highlight/vro1.mp4'
+
+
+
+export type Item = {
+    Name: string,
+    Caption: any,
+    contentPosition: "left" | "right" | "middle",
+    Items: { Name: string, Image: string, Type?: 'Text' | 'Video' | 'Image' }[]
+}
+const items: Item[] = [
+    {
+        Name: "Programa Laboratório de Talentos - Agosto 2023",
+        Caption: "",
+        contentPosition: "left",
+        Items: [
+            
+            
+            {
+                Name: "",
+                Image: pro2
+            },
+            {
+                Name: "",
+                Image: pro1
+            }
+        ]
+    },
+    {
+        Name: "Programa Mentoria Financeira - Novembro 2023",
+        Caption: "",
+        contentPosition: "right",
+        Items: [
+            {
+                Name: "",
+                Image: pro4
+            },
+            {
+                Name: "Learus Vacuum Cleaner",
+                Image: vro1,
+                Type: 'Video'
+            }
+        ]
+    },
+    {
+        Name: "Programa de Preparação de Consultores Empresariais - Abril de 2024",
+        Caption: <> <b>Objectivo:</b> " Desenvolvimento de habilidade sobre técnicas e ferramentas para prestação de serviços de consultoria em gestão e transformação organizacional." <br/><b> Benefícios:</b>      <ul><li>   "Certificado de participação;"</li><li>        Estágio profissional na Everest LDA e numa empresa de contabilidade (aplicável somente aos 4 melhores participantes);</li><li> Mentoria individual com consultores financeiros e de investimentos (aplicável somente aos 4 melhores participantes)."</li></ul></>,
+        contentPosition: "middle",
+        Items: [
+            {
+                Name: "",
+                Image: pro3
+            },
+            {
+                Name: "",
+                Image: pro6
+            }
+        ]
+    }
+]
+
+
+export default items
---
+++ src/Pages/Home/Components/Highlight/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightCaroucel from "./HiglightCarousel";
+
+export const Highlight = () => {
+  return (
+    <section id="highlight" className="highlight">
+      <div className="container">
+
+        <div className="row content">
+            <HighlightCaroucel/>
+        </div>
+      </div>
+    </section>
+  );
+};
