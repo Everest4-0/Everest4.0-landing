9bf3f2e5cbf56118d3516059c41dc84725d711a0
Pedro João <pedrojoaodev@gmail.com>
2024-02-22 14:47:06 +0100

    Navbar fixed

--- src/Shared/Header/Header.tsx
+++ src/Shared/Header/Header.tsx
@@ -1,122 +1,55 @@
 import logoDark from "../../assets/images/logo/logo-dark.png";
+import HeaderList from "./HeaderList";
 
 import "./index.css";
 
 export const Header = () => {
   return (
-    <header id="header" className="fixed-top d-flex align-items-center">
-      <div className="container d-flex justify-content-between">
-        <a href="#" className="logo-icon">
-          {/* <img src={logoDark} className="img-thumbnail" alt="..." /> */}
-        </a>
-        <nav id="navbar" className="navbar">
-          <ul>
-            <li>
-              <a className="nav-link scrollto active" href="#hero">
-                Inicio
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto " href="#highlight">
-                Destaque
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#about">
-                Sobre Nós
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#services">
-                Serviços
-              </a>
-            </li>
-            <li>
-              <a className="nav-link scrollto" href="#courses">
-                Cursos
-              </a>
-            </li>
+    <>
+      <header id="header" className="fixed-top">
+        <nav className="navbar" id="navbar" >
+          <div className="container">
+            <a className="navbar-brand" href="#">
+              <div className="logo-icon"></div>
+            </a>
+            <button
+              className="btn border-0 shadow-none mobile-nav-toggle"
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasNavbar"
+              aria-controls="offcanvasNavbar"
+            >
+              <i className="bi bi-list"></i>
+            </button>
 
-            <li>
-              <a className="nav-link scrollto" href="#pricing">
-                Preços
-              </a>
-            </li>
+            <div
+              className="offcanvas offcanvas-end"
+              tabIndex={-1}
+              id="offcanvasNavbar"
+              aria-labelledby="offcanvasNavbarLabel"
+            >
+              <div className="offcanvas-header">
+                <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+                  Menu
+                </h5>
+                <button
+                  type="button"
+                  className="btn shadow-none"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                >
+                  <i className="bi bi-x-lg"></i>
+                </button>
+              </div>
+              <div className="offcanvas-body">
+                <HeaderList show={true} />
+              </div>
+            </div>
 
-            <li>
-              <a className="nav-link scrollto" href="#team">
-                Nossa equipa
-              </a>
-            </li>
-
-            <li>
-              <a className="nav-link scrollto" href="#faq">
-                {/* App everest4.0 */} FAQ
-              </a>
-            </li>
-            {/*  <li>
-              <a className="nav-link scrollto" href="#about">
-                Anúcions Vagas
-              </a>
-            </li> */}
-
-            {/*  <li className="dropdown">
-              <a href="#">
-                <span>Drop Down</span> <i className="bi bi-chevron-down"></i>
-              </a>
-              <ul>
-                <li>
-                  <a href="#">Drop Down 1</a>
-                </li>
-                <li className="dropdown">
-                  <a href="#">
-                    <span>Deep Drop Down</span>{" "}
-                    <i className="bi bi-chevron-right"></i>
-                  </a>
-                  <ul>
-                    <li>
-                      <a href="#">Deep Drop Down 1</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 2</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 3</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 4</a>
-                    </li>
-                    <li>
-                      <a href="#">Deep Drop Down 5</a>
-                    </li>
-                  </ul>
-                </li>
-                <li>
-                  <a href="#">Drop Down 2</a>
-                </li>
-                <li>
-                  <a href="#">Drop Down 3</a>
-                </li>
-                <li>
-                  <a href="#">Drop Down 4</a>
-                </li>
-              </ul>
-            </li> */}
-
-            <li>
-              <a className="nav-link scrollto" href="#contact">
-                Contactos
-              </a>
-            </li>
-            <li>
-              <a className="getstarted scrollto" href="#about">
-                Entrar
-              </a>
-            </li>
-          </ul>
-          <i className="bi bi-list mobile-nav-toggle"></i>
+            <HeaderList />
+          </div>
         </nav>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
